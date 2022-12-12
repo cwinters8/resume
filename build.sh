a27a7ec9e80bc46c800fd599c38250648dbceca7
Clark Winters <github@clarkwinters.com>
2022-12-11 20:53:28 -0600

    fix build

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ out_dir="output"
 out_html="resume.html"
 out_pdf="resume.pdf"
 
+[ ${ENV} == "ci" ] && out_html="index.html"
+
 out="${out_dir}/${out_html}"
 timestamp=$(date)
 
@@ -15,11 +17,11 @@ pandoc ${in} -f markdown -t html -s -o ${out} -c assets/style.css
 
 [ $? -eq 0 ] && echo "${timestamp}: ${out} built successfully" || ( echo "${timestamp}: html build failed" && exit $? )
 
-[ ${ENV} == "ci" ] && ( echo "can't create pdf in ci yet. stopping here" && exit 0 ) || echo "not ci. continuing to pdf printing"
+# [ ${ENV} == "ci" ] && ( echo "can't create pdf in ci yet. stopping here" && exit 0 ) || echo "not ci. continuing to pdf printing"
 
 in=${out}
 out="${out_dir}/${out_pdf}"
 
-chromium --headless --print-to-pdf=${out} ${in}
+chromium --headless --disable-pdf-tagging --print-to-pdf=${out} ${in}
 
 [ $? -eq 0 ] && echo "${timestamp}: ${out} built successfully" || ( echo "${timestamp}: pdf build failed" && exit $? )
