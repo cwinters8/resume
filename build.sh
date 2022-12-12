65bd58d2d13f289bca74bab963cc96229e3ddddd
Clark Winters <github@clarkwinters.com>
2022-12-11 21:01:04 -0600

    try using google-chrome instead of chromium for pdf

--- build.sh
+++ build.sh
@@ -5,7 +5,13 @@ out_dir="output"
 out_html="resume.html"
 out_pdf="resume.pdf"
 
-[ ${ENV} == "ci" ] && out_html="index.html"
+chromium="chromium"
+
+if [ ${ENV} == "ci" ]; then
+  echo "in ci"
+  out_html="index.html"
+  chromium="google-chrome"
+fi
 
 out="${out_dir}/${out_html}"
 timestamp=$(date)
@@ -16,8 +22,6 @@ cp -R assets ${out_dir}/
 pandoc ${in} -f markdown -t html -s -o ${out} -c assets/style.css
 
 [ $? -eq 0 ] && echo "${timestamp}: ${out} built successfully" || ( echo "${timestamp}: html build failed" && exit $? )
-
-# [ ${ENV} == "ci" ] && ( echo "can't create pdf in ci yet. stopping here" && exit 0 ) || echo "not ci. continuing to pdf printing"
 
 in=${out}
 out="${out_dir}/${out_pdf}"
