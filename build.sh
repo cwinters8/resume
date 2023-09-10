91a8e2ea9f187245a476d6ae5cbf5c8b9783a123
Clark Winters <github@clarkwinters.com>
2023-09-09 21:12:12 -0500

    finish optimizing build script

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-md="resume.md"
 out_dir="output"
 out_html="index.html"
-out_pdf="Clark_Winters-resume.pdf"
+out_pdf="resume.pdf"
 
 custom_dir="custom"
 is_ci=false
@@ -14,8 +13,14 @@ else
   is_ci=true
 fi
 
-function build {
-  local in=$1
+function resume {
+  if [ -z $1 ] || [ -z $2 ]; then
+    echo "two arguments are required."
+    echo "Usage: $0 input_directory_path output_directory_name"
+    echo "Example: $0 ./ main"
+    return 2
+  fi
+  local in=$1/resume.md
   local out_dir=output/$2
   local out="${out_dir}/${out_html}"
   local timestamp=$(date)
@@ -37,47 +42,50 @@ function build {
   [ $? -eq 0 ] && echo "${timestamp}: ${out} built successfully" || ( echo "${timestamp}: pdf build failed" && exit $? )
 }
 
-if [ $1 == "-a" ]; then
-  build ./${md} 
+function cover {
+  if [ -z $1 ] || [ -z $2 ]; then
+    echo "two arguments are required."
+    echo "Usage: $0 input_directory_path output_directory_name"
+    echo "Example: $0 ./custom/Acme Acme"
+  fi
+  local in=$1/cover_letter.pdf
+  local out_dir=output/$2
+
+  if [ -f $in ]; then
+    mkdir -p $out_dir
+    cp $in $out_dir/
+  fi
+}
+
+case $1 in
+  main)
+    resume ./ main
+    ;;
+  -a)
+    resume ./ main
+
+    for dir in $(ls custom)
+    do
+      custom_dir="custom/${dir}"
+
+      resume $custom_dir $dir
+      cover $custom_dir $dir
+    done
+    ;;
+  *)
+    for target in "$@"
+    do
+      if [ $target == "main" ]; then
+        resume ./ main
+      else
+        custom_dir=custom/$target
+        resume $custom_dir $target
+        cover $custom_dir $target
+      fi
+    done
+    ;;
+esac
+
+if $is_ci; then
+  rm -f $out_dir/**/$out_html $out_dir/**/$out_pdf
 fi
-
-
-
-pandoc ${in} -f markdown -t html -s -o ${out} -c assets/style.css
-
-[ $? -eq 0 ] && echo "${timestamp}: ${out} built successfully" || ( echo "${timestamp}: html build failed" && exit $? )
-
-in=${out}
-out="${out_dir}/${out_pdf}"
-
-chromium --headless --print-to-pdf-no-header --disable-pdf-tagging --print-to-pdf=${out} ${in}
-
-[ $? -eq 0 ] && echo "${timestamp}: ${out} built successfully" || ( echo "${timestamp}: pdf build failed" && exit $? )
-
-for dir in $(ls custom)
-do
-  custom_dir="custom/${dir}"
-  resume_in="${custom_dir}/resume.md"
-  resume_out_dir="${out_dir}/${dir}"
-  resume_out_pdf="${resume_out_dir}/${out_pdf}"
-  resume_out_html="${resume_out_dir}/${out_html}"
-  cover_letter="${custom_dir}/cover_letter.pdf"
-
-  mkdir -p ${resume_out_dir}
-  rm -rf ${resume_out_dir}/*
-  cp -R assets ${resume_out_dir}/
-
-  pandoc ${resume_in} -f markdown -t html -s -o ${resume_out_html} -c assets/style.css
-  [ $? -eq 0 ] && echo "${timestamp}: ${resume_out_html} built successfully" || ( echo "${timestamp}: html build failed" && exit $? )
-
-  chromium --headless --print-to-pdf-no-header --disable-pdf-tagging --print-to-pdf=${resume_out_pdf} ${resume_out_html}
-  [ $? -eq 0 ] && echo "${timestamp}: ${resume_out_pdf} built successfully" || ( echo "${timestamp}: pdf build failed" && exit $? )
-
-  if $is_ci; then
-    rm -f ${resume_out_html} ${resume_out_pdf}
-  fi
-
-  if [ -f "${cover_letter}" ]; then
-    cp ${cover_letter} ${resume_out_dir}/
-  fi
-done
