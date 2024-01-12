fa6712268a9662e422abc58dc8c4813b725f4fa8
Clark Winters <github@clarkwinters.com>
2024-01-11 23:27:44 -0600

    remove handling for custom resumes etc

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ out_dir="output"
 out_html="index.html"
 out_pdf="resume.pdf"
 
-custom_dir="custom"
 is_ci=false
 
 if [[ ${ENV} != "ci" ]]; then
@@ -13,84 +12,22 @@ else
   is_ci=true
 fi
 
-function resume {
-  if [ -z $1 ] || [ -z $2 ]; then
-    echo "two arguments are required."
-    echo "Usage: $0 input_directory_path output_directory_name"
-    echo "Example: $0 ./ main"
-    return 2
-  fi
-  local in=$1/resume.md
-  local out_dir=output/$2
-  local out="${out_dir}/${out_html}"
-  local timestamp=$(date)
-  
-  mkdir -p ${out_dir}
-  rm -rf ${out_dir}/*
-  
-  cp -R assets ${out_dir}/
-  
-  pandoc ${in} -f markdown -t html -s -o ${out} -c assets/style.css
+in="resume.md"
+out="${out_dir}/${out_html}"
+timestamp=$(date)
 
-  [ $? -eq 0 ] && echo "${timestamp}: ${out} built successfully" || ( echo "${timestamp}: html build failed" && exit $? )
+mkdir -p ${out_dir}
+rm -rf ${out_dir}/*
 
-  in=${out}
-  out="${out_dir}/${out_pdf}"
+cp -R assets ${out_dir}/
 
-  chromium --headless --no-pdf-header-footer --disable-pdf-tagging --print-to-pdf=${out} ${in}
+pandoc ${in} -f markdown -t html -s -o ${out} -c assets/style.css
 
-  [ $? -eq 0 ] && echo "${timestamp}: ${out} built successfully" || ( echo "${timestamp}: pdf build failed" && exit $? )
-}
+[ $? -eq 0 ] && echo "${timestamp}: ${out} built successfully" || ( echo "${timestamp}: html build failed" && exit $? )
 
-function cover {
-  if [ -z $1 ] || [ -z $2 ]; then
-    echo "two arguments are required."
-    echo "Usage: $0 input_directory_path output_directory_name"
-    echo "Example: $0 ./custom/Acme Acme"
-  fi
-  local in=$1/cover_letter.pdf
-  local out_dir=output/$2
+in=${out}
+out="${out_dir}/${out_pdf}"
 
-  mkdir -p $out_dir
+chromium --headless --no-pdf-header-footer --disable-pdf-tagging --print-to-pdf=${out} ${in}
 
-  if [ -f $in ]; then
-    cp $in $out_dir/
-  elif [ -f "$1/cover.md" ]; then
-    in="$1/cover.md"
-    out="$out_dir/cover.html"
-    pandoc ${in} -f markdown -t html -s -o ${out} -c assets/style.css
-
-    in=$out
-    out="$out_dir/cover.pdf"
-    chromium --headless --no-pdf-header-footer --disable-pdf-tagging --print-to-pdf=${out} ${in}
-  fi
-}
-
-case $1 in
-  main)
-    resume ./ main
-    ;;
-  -a)
-    resume ./ main
-
-    for dir in $(ls custom)
-    do
-      custom_dir="custom/${dir}"
-
-      resume $custom_dir $dir
-      cover $custom_dir $dir
-    done
-    ;;
-  *)
-    for target in "$@"
-    do
-      if [ $target == "main" ]; then
-        resume ./ main
-      else
-        custom_dir=custom/$target
-        resume $custom_dir $target
-        cover $custom_dir $target
-      fi
-    done
-    ;;
-esac
+[ $? -eq 0 ] && echo "${timestamp}: ${out} built successfully" || ( echo "${timestamp}: pdf build failed" && exit $? )
